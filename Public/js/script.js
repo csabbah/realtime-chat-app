735e00d2543526861f97d17a8cc8f6c6a55ad841
Csabbah <csabbah@ryerson.ca>
2022-08-05 14:41:39 -0400

    Setup socket.io on the backend and to be used on the client side

---
+++ Public/js/script.js
@@ -0,0 +1,13 @@
+const socket = io();
+// We have access to io() because of this in chat.html:
+// <script src="/socket.io/socket.io.js"></script>
+
+// Whenever we get this 'message' event
+// these events are established in the server.js file
+socket.on('object', (object) => {
+  console.log(object);
+});
+
+socket.on('message', (message) => {
+  console.log(message);
+});
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const http = require('http');
+const server = http.createServer(app);
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
 // Example route
 // app.use('/test', (res, req) => {
 //   req.send('hi');
@@ -10,7 +16,16 @@ const app = express();
 // Set static folder
 app.use(express.static(path.join(__dirname, './public')));
 
+// Run when client connects
+io.on('connection', (socket) => {
+  // This of this as opening a door between server and client
+  // we can emit events
+  console.log('New WS connection...');
+  socket.emit('object', { welcome: 'Welcome to ChatCord' });
+  socket.emit('message', 'Welcome to ChatCord');
+}); // Listen for a connection
+
 // Local and or environment variable named port
 const PORT = 3000 || process.env.PORT;
 // Run the server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
